a577b52f6ed5b2c8f3e2f7659fa66cd8cdc814e5
89not72 <d1185456@o365.fcu.edu.tw>
2025-12-31 16:33:07 +0800

    Update MyStack.java

--- MyStack.java
+++ MyStack.java
@@ -1,38 +1,46 @@
 import java.util.LinkedList;
 
-// 根據 Lab-8 PDF 建議，繼承 LinkedList 來實作
+// 這是主要的 Stack 類別，必須是 public
 public class MyStack<T> extends LinkedList<T> {
 
-    // 建構子
     public MyStack() {
         super();
     }
 
-    // 將項目推入 Stack 頂端
-    // LinkedList 的 push() 等同於 addFirst()
     @Override
     public void push(T item) {
         super.push(item);
     }
 
-    // 取出並移除 Stack 頂端的項目
-    // LinkedList 的 pop() 等同於 removeFirst()
     @Override
     public T pop() {
         return super.pop();
     }
 
-    // 判斷 Stack 是否為空
-    // 直接使用繼承自 LinkedList (Collection) 的 isEmpty()
     @Override
     public boolean isEmpty() {
         return super.isEmpty();
     }
 
-    // 取得 Stack 大小
-    // 直接使用繼承自 LinkedList 的 size()
     @Override
     public int size() {
         return super.size();
+    }
+}
+
+// --- Person 類別寫在這裡 (MyStack 的大括號之外) ---
+// 注意：這裡不能加 public，因為一個檔案只能有一個 public class
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
     }
 }
